02e07847b2d041bbf207d194bf66080fe6fb2cda
万潇 <wanxiao@moxiaodeMacBook-Air.local>
2026-07-15 23:59:37 +0800

    feat: rename navbar title

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -78,7 +78,7 @@ export const siteConfig: SiteConfig = {
 			alt: "潇拾壹",
 		},
 		// 导航栏标题
-		title: "舌甘舌甘一笑",
+		title: "潇拾壹的Blog",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
